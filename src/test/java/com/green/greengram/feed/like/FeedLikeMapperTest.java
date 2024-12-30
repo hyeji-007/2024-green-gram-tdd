b86dd482e7d3f890150d5af0c8c0e3420f1de8b6
KwonHyeji <kwonhyeji007@gmail.com>
2024-12-30 11:11:43 +0900

    2024-12-30 class

--- src/test/java/com/green/greengram/feed/like/FeedLikeMapperTest.java
+++ src/test/java/com/green/greengram/feed/like/FeedLikeMapperTest.java
@@ -1,6 +1,7 @@
 package com.green.greengram.feed.like;
 
 import com.green.greengram.feed.like.model.FeedLikeReq;
+import com.green.greengram.feed.like.model.FeedLikeVo;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,19 +14,26 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @ActiveProfiles("test") //yaml 적용되는 파일 선택 (application-test.yml)
 @MybatisTest //Mybatis Mapper Test이기 때문에 작성 >> Mapper 들이 전부 객체화
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 //테스트를 기본적으로 메모리 데이터베이스 (H2)를 사용해서 하는데 메모리 데이터베이스로 교체하지 않겠다.
-// 즉, 우리가 원래 쓰는 데이터베이스로 테스트를 진행하겠다.
+//즉, 우리가 원래 쓰는 데이터베이스로 테스트를 진행하겠다.
 //@TestInstance(TestInstance.Lifecycle.PER_CLASS) //기본값은 PER_METHOD 메소드마다 객체를 만든다, static 삭제 가능
+//PER_CLASS 설정이 있으면 static을 제거해도 된다.
+//테스트 객체를 딱 하나만 만든다.
 class FeedLikeMapperTest {
 
     @Autowired // TODO: 스프링 컨테이너가 DI해주는게 맞는지 확인, @Autowired는 필드 주입 방식이다.
     FeedLikeMapper feedLikeMapper; // @Autowired >> 필드 주입 방식의 DI가 된다.
     // DI 방법: 생성자/필드 주입/세터 주입이 있는데 Test는 생성자를 통한 DI가 안된다.
+
+    @Autowired
+    FeedLikeTestMapper feedLikeTestMapper;
 
     static final long FEED_ID_1 = 1L;
     static final long FEED_ID_5 = 5L;
@@ -43,9 +51,9 @@ class FeedLikeMapperTest {
         @AfterAll - 모든 테스트 실행 후에 최초 한번 실행
      */
 
-    // @BeforeAll - 테스트 메소드 실행되기 전에 최초 딱 한번만 실행되는 메소드
-    // 테스트 메소드마다 테스트 객체가 만들어지면 BeforeAll 메소드는 static 메소드여야 한다.
-    // 한 테스트 객체가 만들어지면 non-static 메소드여야 한다.
+    // @BeforeAll - 모든 테스트 메소드 실행되기 전에 최초 딱 한번만 실행되는 메소드
+    // 테스트 메소드마다 테스트 객체가 만들어지면 BeforeAll 메소드는 무조건 static 메소드여야 한다.
+    // 한 테스트 객체가 만들어지면 non-static 메소드일 수 있다.
     @BeforeAll //최초 딱 한번만 실행
     static void initData() {
         existedData.setFeedId(FEED_ID_1);
@@ -72,13 +80,24 @@ class FeedLikeMapperTest {
     }
 
     @Test
-    void insFeedLikeNormal() {
+    void insFeedLike() {
 
         //when
+        List<FeedLikeVo> actualFeedLikeListBefore = feedLikeTestMapper.selFeedLikeAll(); // insert 전 기존 튜플 수
+        FeedLikeVo actualFeedLikeVoBefore = feedLikeTestMapper.selFeedLikeByFeedIdAndUserId(notExistedData); //insert 전 WHERE절에 PK로 데이터를 가져옴
         int actualAffectedRows = feedLikeMapper.insFeedLike(notExistedData);
+        List<FeedLikeVo> actualFeedLikeListAfter = feedLikeTestMapper.selFeedLikeAll(); //insert 후 WHERE절에 PK로 데이터를 가져옴
+        FeedLikeVo actualFeedLikeVoAfter = feedLikeTestMapper.selFeedLikeByFeedIdAndUserId(notExistedData); //insert 후 튜플 수
 
         //then
-        assertEquals(1, actualAffectedRows);
+        assertAll(
+                  () -> assertEquals(actualFeedLikeListBefore.size() + 1, actualFeedLikeListAfter.size())
+                , () -> assertNull(actualFeedLikeVoBefore)
+                , () -> assertNotNull(actualFeedLikeVoAfter)
+                , () -> assertEquals(1, actualAffectedRows)
+                , () -> assertEquals(notExistedData.getFeedId(), actualFeedLikeVoAfter.getFeedId())
+                , () -> assertEquals(notExistedData.getUserId(), actualFeedLikeVoAfter.getUserId())
+        );
     }
 
     @Test
@@ -89,7 +108,7 @@ class FeedLikeMapperTest {
     }
 
     @Test
-    void delFeedLikeNormal() {
+    void delFeedLike() {
         int actualAffectedRows = feedLikeMapper.delFeedLike(existedData);
         assertEquals(1, actualAffectedRows);
     }
---
+++ src/test/java/com/green/greengram/feed/like/FeedLikeTestMapper.java
@@ -0,0 +1,19 @@
+package com.green.greengram.feed.like;
+
+import com.green.greengram.feed.like.model.FeedLikeReq;
+import com.green.greengram.feed.like.model.FeedLikeVo;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface FeedLikeTestMapper {
+    @Select("SELECT * FROM feed_like WHERE feed_id = #{feedId} AND user_id = #{userId}") //객체가 하나일 때는 null값 넘어올 수 있다.
+    FeedLikeVo selFeedLikeByFeedIdAndUserId(FeedLikeReq p);
+
+    @Select("SELECT * FROM feed_like") //List때에는 null값 넘어오지 않는다. 0개짜리 ArrayList가 넘어온다.
+    List<FeedLikeVo> selFeedLikeAll();
+
+
+}
